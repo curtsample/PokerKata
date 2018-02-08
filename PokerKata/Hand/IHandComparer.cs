59ec1283dac024ad436b73117d8bd6014dbd7b9f
Curtis Sample <curtis.sample@gmail.com>
2018-02-08 23:58:52 +0000

    Skeleton for comparing two hands

---
+++ PokerKata/Hand/IHandComparer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerKata {
+   public interface IHandComparer {
+      HandCompareResult Compare(Hand firstHand, Hand secondHand);
+   }
+}
