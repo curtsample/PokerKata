e82dbf316e5fa4abfcf766006f752ec5902099b6
Curtis Sample <curtis.sample@gmail.com>
2018-02-09 00:03:27 +0000

    Add an evaluator to FiveCardHandComparer

--- PokerKata/Hand/FiveCardHandComparer.cs
+++ PokerKata/Hand/FiveCardHandComparer.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace PokerKata {
    public class FiveCardHandComparer : IHandComparer {
+      private readonly IHandEvaluator _evaluator;
+
+      public FiveCardHandComparer(IHandEvaluator evaluator) {
+         _evaluator = evaluator;
+      }
+
       public HandCompareResult Compare(Hand firstHand, Hand secondHand) {
          throw new NotImplementedException();
       }
