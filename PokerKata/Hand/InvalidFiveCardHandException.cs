da9e3e4f636e01e82358d87df6efb06897316d38
Curtis Sample <curtis.sample@gmail.com>
2018-02-09 00:56:07 +0000

    InvalidFiveCardHandException

---
+++ PokerKata/Hand/InvalidFiveCardHandException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerKata {
+   public class InvalidFiveCardHandException : Exception {
+      public int NumberOfCards { get; private set; }
+
+      public InvalidFiveCardHandException(int numberOfCards)
+         : base($"Invalid five card hand -> ${numberOfCards} provided") { }
+   }
+}
