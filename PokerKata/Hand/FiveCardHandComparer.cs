16ce9d0ac85c89b4e6adbd2695bb12ac8154a863
Curtis Sample <curtis.sample@gmail.com>
2018-02-15 00:31:38 +0000

    Remove unneeded code

--- PokerKata/Hand/FiveCardHandComparer.cs
+++ PokerKata/Hand/FiveCardHandComparer.cs
@@ -41,35 +41,5 @@ namespace PokerKata {
          // should not reach here (famous last words...)
          throw new Exception($"Unable to compare the two hands provided for rank ${rank}");
       }
-
-      private HandCompareResult ComparePairedRanks(Hand firstHand, Hand secondHand) {
-         var firstHandPairedCards = firstHand.GetPairedCards();
-         var secondHandPairedCards = secondHand.GetPairedCards();
-
-         var firstHandPairRank = firstHandPairedCards.First().Rank;
-         var secondHandPairRank = secondHandPairedCards.First().Rank;
-
-         var firstHandKickers = firstHand.Cards.Except(firstHandPairedCards).OrderByDescending(o => o.Rank);
-         var secondHandKickers = secondHand.Cards.Except(secondHandPairedCards).OrderByDescending(o => o.Rank);
-
-         if (firstHandPairRank != secondHandPairRank) {
-            return firstHandPairRank > secondHandPairRank
-               ? HandCompareResult.FirstHandWins
-               : HandCompareResult.SecondHandWins;
-         }
-
-         for (int i = 0; i < firstHandKickers.Count(); i++) {
-            var firstKicker = firstHandKickers.ElementAt(i).Rank;
-            var secondKicker = secondHandKickers.ElementAt(i).Rank;
-
-            if (firstKicker != secondKicker) {
-               return firstKicker > secondKicker
-                  ? HandCompareResult.FirstHandWins
-                  : HandCompareResult.SecondHandWins;
-            }
-         }
-
-         return HandCompareResult.Split;
-      }
    }
 }
