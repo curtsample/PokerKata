33bd8a4823100ccde1d1cf1e3aa8bfa3c1eb841c
Curtis Sample <curtis.sample@gmail.com>
2018-02-15 01:13:59 +0000

    HandComparerFactory and failing test

---
+++ PokerKata.Tests/Hand/HandComparerFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerKata.Tests {
+   [TestClass]
+   public class HandComparerFactoryTests {
+      private HandComparerFactory _factory;
+
+      [TestInitialize]
+      public void BeforeEach() {
+         _factory = new HandComparerFactory();
+      }
+
+      [TestMethod]
+      public void GetComparer_Success() {
+         // arrange
+         var testData = new[] {
+            new { Rank = HandRank.StraightFlush, ExpectedType = typeof(HighestRankComparer) },
+            new { Rank = HandRank.Flush, ExpectedType = typeof(HighestRankComparer) },
+            new { Rank = HandRank.Straight, ExpectedType = typeof(HighestRankComparer) },
+            new { Rank = HandRank.FourOfAKind, ExpectedType = typeof(PairedCardComparer) },
+            new { Rank = HandRank.ThreeOfAKind, ExpectedType = typeof(PairedCardComparer) },
+            new { Rank = HandRank.Pair, ExpectedType = typeof(PairedCardComparer) },
+         };
+
+         // act & assert
+         foreach (var test in testData) {
+            Assert.IsInstanceOfType(_factory.GetComparer(test.Rank), test.ExpectedType);
+         }         
+      }
+   }
+}
---
+++ PokerKata/Hand/HandComparerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerKata {
+   public class HandComparerFactory : IHandComparerFactory {
+      public IHandComparer GetComparer(HandRank rank) {
+         throw new NotImplementedException();
+      }
+   }
+}
