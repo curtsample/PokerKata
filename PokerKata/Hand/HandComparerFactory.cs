0d8f61f10c796d9393456cd8170abe668f50be76
Curtis Sample <curtis.sample@gmail.com>
2018-02-15 01:17:19 +0000

    Passing test!

--- PokerKata/Hand/HandComparerFactory.cs
+++ PokerKata/Hand/HandComparerFactory.cs
@@ -7,7 +7,20 @@ using System.Threading.Tasks;
 namespace PokerKata {
    public class HandComparerFactory : IHandComparerFactory {
       public IHandComparer GetComparer(HandRank rank) {
-         throw new NotImplementedException();
+         if (_comparerMap.ContainsKey(rank)) {
+            return Activator.CreateInstance(_comparerMap[rank]) as IHandComparer;
+         }
+
+         throw new Exception($"Could not find a valid comparer for rank {rank}");
       }
+
+      private static readonly IDictionary<HandRank, Type> _comparerMap = new Dictionary<HandRank, Type> {
+         { HandRank.StraightFlush, typeof(HighestRankComparer) },
+         { HandRank.Flush, typeof(HighestRankComparer) },
+         { HandRank.Straight, typeof(HighestRankComparer) },
+         { HandRank.FourOfAKind, typeof(PairedCardComparer) },
+         { HandRank.ThreeOfAKind, typeof(PairedCardComparer) },
+         { HandRank.Pair, typeof(PairedCardComparer) }
+      };
    }
 }
