c97f33aaed5875506feeded23c27063b3e31032e
Curtis Sample <curtis.sample@gmail.com>
2018-02-15 00:30:16 +0000

    PairedCardComparer test class

---
+++ PokerKata.Tests/Hand/PairedCardComparerTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerKata.Tests {
+   [TestClass]
+   public class PairedCardComparerTests {
+      private PairedCardComparer _comparer;
+
+      [TestInitialize]
+      public void BeforeEach() {
+         _comparer = new PairedCardComparer();
+      }
+
+      [TestMethod]
+      public void Compare_WithFourOfAKind_Success() {
+         // arrange
+         var winningPair = new FiveCardPokerHand(new List<Card> {
+            new Card(Rank.Ace, Suit.Spades),
+            new Card(Rank.Ace, Suit.Diamonds),
+            new Card(Rank.Ace, Suit.Hearts),
+            new Card(Rank.Ace, Suit.Clubs),
+            new Card(Rank.Eight, Suit.Hearts)
+         });
+
+         var losingPair = new FiveCardPokerHand(new List<Card> {
+            new Card(Rank.Eight, Suit.Spades),
+            new Card(Rank.Eight, Suit.Diamonds),
+            new Card(Rank.Eight, Suit.Hearts),
+            new Card(Rank.Eight, Suit.Clubs),
+            new Card(Rank.Ace, Suit.Hearts)
+         });
+
+         var winningKicker = new FiveCardPokerHand(new List<Card> {
+            new Card(Rank.Ace, Suit.Spades),
+            new Card(Rank.Ace, Suit.Diamonds),
+            new Card(Rank.Ace, Suit.Hearts),
+            new Card(Rank.Ace, Suit.Clubs),
+            new Card(Rank.King, Suit.Hearts)
+         });
+
+         var losingKicker = new FiveCardPokerHand(new List<Card> {
+            new Card(Rank.Ace, Suit.Spades),
+            new Card(Rank.Ace, Suit.Diamonds),
+            new Card(Rank.Ace, Suit.Hearts),
+            new Card(Rank.Ace, Suit.Clubs),
+            new Card(Rank.Eight, Suit.Hearts)
+         });
+
+         var testData = new[] {
+            new TestData(winningPair, losingPair, HandCompareResult.FirstHandWins),
+            new TestData(losingPair, winningPair, HandCompareResult.SecondHandWins),
+            new TestData(winningPair, winningPair, HandCompareResult.Split),
+            new TestData(winningKicker, losingKicker, HandCompareResult.FirstHandWins),
+            new TestData(losingKicker, winningKicker, HandCompareResult.SecondHandWins)
+         };
+
+         // act & assert
+         foreach (var test in testData) {
+            Assert.AreEqual(test.ExpectedResult, _comparer.Compare(test.First, test.Second));
+         }
+      }
+
+      private class TestData {
+         public Hand First { get; private set; }
+         public Hand Second { get; private set; }
+         public HandCompareResult ExpectedResult { get; private set; }
+
+         public TestData(Hand first, Hand second, HandCompareResult expectedResult) {
+            First = first;
+            Second = second;
+            ExpectedResult = expectedResult;
+         }
+      }
+   }
+}
---
+++ PokerKata/Hand/PairedCardComparer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerKata {
+   public class PairedCardComparer : IHandComparer {
+      public HandCompareResult Compare(Hand firstHand, Hand secondHand) {
+         var firstHandPairedCards = firstHand.GetPairedCards();
+         var secondHandPairedCards = secondHand.GetPairedCards();
+
+         var firstHandPairRank = firstHandPairedCards.First().Rank;
+         var secondHandPairRank = secondHandPairedCards.First().Rank;
+
+         var firstHandKickers = firstHand.Cards.Except(firstHandPairedCards).OrderByDescending(o => o.Rank);
+         var secondHandKickers = secondHand.Cards.Except(secondHandPairedCards).OrderByDescending(o => o.Rank);
+
+         if (firstHandPairRank != secondHandPairRank) {
+            return firstHandPairRank > secondHandPairRank
+               ? HandCompareResult.FirstHandWins
+               : HandCompareResult.SecondHandWins;
+         }
+
+         for (int i = 0; i < firstHandKickers.Count(); i++) {
+            var firstKicker = firstHandKickers.ElementAt(i).Rank;
+            var secondKicker = secondHandKickers.ElementAt(i).Rank;
+
+            if (firstKicker != secondKicker) {
+               return firstKicker > secondKicker
+                  ? HandCompareResult.FirstHandWins
+                  : HandCompareResult.SecondHandWins;
+            }
+         }
+
+         return HandCompareResult.Split;
+      }
+   }
+}
